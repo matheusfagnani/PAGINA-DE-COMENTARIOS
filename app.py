3c517b84de3f891d5222099862282edcd3886aa5
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-04-08 11:01:51 -0300

    unicorn

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def add_curtida(codigo):
 
 
 
-
-app.run()
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080)
--- data/conexão.py
+++ data/conexão.py
@@ -1,9 +1,9 @@
 import mysql.connector
 class Conexao:
     def criar_conexao():
-        conexao= mysql.connector.connect(host="10.110.134.2", 
-                                        port=3306,
-                                        user="root",
-                                    password="root",
+        conexao= mysql.connector.connect(host="bdgodofredo-alexstocco-93db.b.aivencloud.com", 
+                                        port=27974,
+                                        user="3ds",
+                                    password="banana",
                                     database="Cadastro_comentarios")
         return conexao
